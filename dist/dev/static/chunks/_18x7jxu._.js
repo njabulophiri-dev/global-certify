ee83fec304bca00b36723a07fc99bc87f52eef7f
Njabulo Phiri <donxjay.cs@gmail.com>
2026-08-31 10:12:54 +0200

    Ignore build artifacts: add dist/ to .gitignore and untrack

--- dist/dev/static/chunks/_18x7jxu._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/app_globals_0yg4wg8.css","static/chunks/node_modules_0b31m4i._.js","static/chunks/components_Navbar_tsx_0316qgo._.js"],
-    source: "entry"
-});
